d0fc2b8200c3989189e0a2ab947ac416e49e1ae3
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-03-22 15:57:50 +0100

    added to the db: the product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_111319) do
+ActiveRecord::Schema.define(version: 2020_03_22_145641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "description"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "price"
+    t.boolean "bought"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
