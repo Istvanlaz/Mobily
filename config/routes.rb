856ded091a5aaa3ccbf9f8a0d4c37b118b917c0a
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-03-23 11:33:05 +0100

    added a controller to wishlist with the following actions (index show new edit destroy) and of course the views that come with it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'wishlists/index'
+  get 'wishlists/show'
+  get 'wishlists/new'
+  get 'wishlists/edit'
+  get 'wishlists/destroy'
   get 'products/index'
   get 'products/show'
   get 'products/new'
