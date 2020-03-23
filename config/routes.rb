33d62d07632e99f116c0b403d7c32b9524ab289e
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-03-23 11:34:33 +0100

    same for the shoping cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'shopingcarts/index'
+  get 'shopingcarts/show'
+  get 'shopingcarts/new'
+  get 'shopingcarts/edit'
+  get 'shopingcarts/destroy'
   get 'wishlists/index'
   get 'wishlists/show'
   get 'wishlists/new'
