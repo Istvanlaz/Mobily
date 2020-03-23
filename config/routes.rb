6e2b449e60771d6a1553720cef17434fc827cd3b
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-03-23 11:31:05 +0100

    added a controller to product with the following actions (index show new edit destroy) and of course the views that come with it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
