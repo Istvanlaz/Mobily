33ea4a269f22b388470bfbb6e6f1263c4fa52098
Istvan Lazar <istvanlaz@gmail.com>
2020-03-26 19:16:42 +0100

    sign-up functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'pages#home'
+
   resources :shopingcarts, only: [:index, :show, :new, :edit, :destroy]
   resources :wishlists, only: [:index, :show, :new, :edit, :destroy]
   resources :products, only: [:index, :show, :new, :create, :edit, :destroy]
-  devise_for :users
-  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
