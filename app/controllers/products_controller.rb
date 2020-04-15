04604f2fd2bd578574d56a88a4b8a33e1c7a166e
Istvan Lazar <istvanlaz@gmail.com>
2020-04-15 14:34:15 +0200

    functionning in console but not in directly in app.. NO MERGE YET.. switching back to gem alternative

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,18 +19,49 @@ class ProductsController < ApplicationController
 
   def new
     @categories = policy_scope(Category)
-    authorize @product = Product.new
+    session[:product_params] ||= {}
+    authorize @product = Product.new(session[:product_params])
+    @product.current_step = session[:product_step]
     @product.user = current_user
   end
 
   def create
-    authorize @product = Product.new(product_params)
+    @categories = policy_scope(Category)
+
+    # authorize @product = Product.new(params_product)
+    # @product.user = current_user
+    # if @product.save!
+    #   redirect_to newest_show_path(@product), notice: "Product has been successfully added to our database"
+    # else
+    #   render :new
+    # end
+        # binding.pry
+    session[:product_params].deep_merge!(params_product) if params_product
+    authorize @product = Product.new(session[:product_params])
+    @product.current_step = session[:product_step]
     @product.user = current_user
-    if @product.save!
-      redirect_to newest_show_path(@product), notice: "Product has been successfully added to our database"
-    else
-      render :new
+    if @product.valid?
+      if params[:back_button]
+        @product.previous_step
+      elsif @product.last_step?
+        if @product.all_valid?
+          @product.save!
+          flash[:notice] = 'Your product was created successfully'
+          redirect_to newest_products_path && return
+        end
+      else
+        @product.next_step
+      end
     end
+    session[:product_step] = @product.current_step
+
+    if @product.new_record?
+      return render :new
+    else
+      session[:product_step] = session[:product_params] = nil
+    end
+
+    # authorize @category = Category.find(params[:category_id])
   end
 
   def edit
@@ -40,7 +71,7 @@ class ProductsController < ApplicationController
 
   def update
     authorize @product = Product.find(params[:id])
-    if @product.update(product_params)
+    if @product.update(params_product)
       redirect_to newest_show_path(@product), notice: "Product has been successfully updated"
     else
       # @product.errors
@@ -61,7 +92,8 @@ class ProductsController < ApplicationController
     authorize @category = Category.find(params[:category_id])
   end
 
-  def product_params
-    params.require(:product).permit(:name, :price, :description, :category_id, :sub_category_id, :user, :id)
+  def params_product
+    params.require(:product).permit(:name, :price, :description, :category, :category_id,
+                                    :sub_category, :sub_category_id, :user, :user_id, :id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  attr_writer :current_step
+
   belongs_to :user, optional: true
   belongs_to :sub_category, optional: true
   belongs_to :category, inverse_of: :products
@@ -7,6 +9,8 @@ class Product < ApplicationRecord
   # validates :name_length
   validates_length_of :name, in: 3..40, message: "must contain between 3 and 40 characters."
 
+  validates_presence_of :name, :category_id, if: lambda { |e| e.current_step == "card" }
+  validates_presence_of :sub_category_id, :description, :price, if: lambda { |e| e.current_step == "details" }
   # validates :category_id, :sub_category_id, presence: true
   # has_and_belongs_to_many :shoping_cart
 
@@ -15,4 +19,35 @@ class Product < ApplicationRecord
 
   #   errors.add :name, "must contain between 3 and 40 characters."
   # end
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def steps
+    %w[card details confirmation]
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step) + 1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step) - 1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  def all_valid?
+    steps.all? do |step|
+      self.current_step = step
+      valid?
+    end
+  end
 end
