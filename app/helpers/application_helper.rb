a4f2f0b9816708c670e7e70a41bca003b38c3158
Istvan Lazar <istvanlaz@gmail.com>
2020-05-15 17:07:36 +0200

    rendered categories banner directly into app layout && created an app_helper to have a focus on active class for banner, categories && sub_categories -- half way done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def active_banner(link_path)
+    current_page?(link_path) ? "link-active" : ""
+  end
+
+  def active_sub_cat(link_path)
+    current_page?(link_path) ? "sub-cat-active" : ""
+  end
+
+  def active_cat(link_path)
+    current_page?(link_path) ? "cat-active" : ""
+  end
 end
