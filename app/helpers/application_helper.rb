c497231e68d7a23d5fca6ebf301d6479ecfeaf79
Istvan Lazar <istvanlaz@gmail.com>
2020-05-19 22:47:43 +0200

    bug fixed with sub-c-banner btns padding && rendered the footer across the whole app && started using method helpers to track routing and have the categories banner displaying the correct info on focus (75% done - banner complete -- needs for sub-banners)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,40 @@
 module ApplicationHelper
-  def active_banner(link_path)
-    current_page?(link_path) ? "link-active" : ""
+  # def active_banner(link_path)
+  #   current_page?(link_path) ? "link-active" : ""
+  # end
+
+  # def active_banner(link_path, control)
+  #   if current_page?(link_path)
+  #     "link-active"
+  #   elsif controller.include?(params[control])
+  #     "link-active"
+  #   else
+  #     ""
+  #   end
+  # end
+
+  def active_banner(controller_name)
+    request.controller_class == controller_name ? "link-active" : ""
+  end
+
+  # def active_category_search(link_path)
+  #   current_page?(link_path) ? "link-active" : ""
+  # end
+
+  def active_category_search(cat_id)
+    full_path = request.fullpath
+    shortened_path = full_path.split("/")
+    shortened_path[2].to_i == cat_id && shortened_path[1] == "categories" ? "link-active" : ""
+  end
+
+  def active_cat(controller, cat_id)
+    # current_page?(link_path) ? "cat-active" : ""
+    full_path = request.fullpath
+    shortened_path = full_path.split("/")
+    shortened_path[2].to_i == cat_id && shortened_path[1] == controller ? "cat-active" : ""
   end
 
   def active_sub_cat(link_path)
     current_page?(link_path) ? "sub-cat-active" : ""
-  end
-
-  def active_cat(link_path)
-    current_page?(link_path) ? "cat-active" : ""
   end
 end
