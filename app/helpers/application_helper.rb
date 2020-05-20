f980b084619c4088c939df5f483806e197ecf54e
Istvan Lazar <istvanlaz@gmail.com>
2020-05-20 15:56:05 +0200

    application helpers functional across all categories / sub_categories / best_deals / newest_products

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,33 +1,15 @@
 module ApplicationHelper
-  # def active_banner(link_path)
-  #   current_page?(link_path) ? "link-active" : ""
-  # end
-
-  # def active_banner(link_path, control)
-  #   if current_page?(link_path)
-  #     "link-active"
-  #   elsif controller.include?(params[control])
-  #     "link-active"
-  #   else
-  #     ""
-  #   end
-  # end
-
   def active_banner(controller_name)
     request.controller_class == controller_name ? "link-active" : ""
   end
 
-  # def active_category_search(link_path)
-  #   current_page?(link_path) ? "link-active" : ""
-  # end
-
-  def active_category_search(cat_id)
+  def active_category_banner(cat_id)
     full_path = request.fullpath
     shortened_path = full_path.split("/")
     shortened_path[2].to_i == cat_id && shortened_path[1] == "categories" ? "link-active" : ""
   end
 
-  def active_cat(controller, cat_id)
+  def active_on_page_category(controller, cat_id)
     # current_page?(link_path) ? "cat-active" : ""
     full_path = request.fullpath
     shortened_path = full_path.split("/")
@@ -35,6 +17,6 @@ module ApplicationHelper
   end
 
   def active_sub_cat(link_path)
-    current_page?(link_path) ? "sub-cat-active" : ""
+    current_page?(link_path) ? "cat-active" : ""
   end
 end
