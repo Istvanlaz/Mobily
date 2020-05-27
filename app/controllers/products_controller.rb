c4f71c3627af8a60ccbae701347f900947c8f52d
Istvan Lazar <istvanlaz@gmail.com>
2020-05-27 12:16:50 +0200

    edit product construction half-way done needs bugs fixing for session data recollection"

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,42 +28,6 @@ class ProductsController < ApplicationController
   def create
     @categories = policy_scope(Category)
 
-    # authorize @product = Product.new(params_product)
-    # @product.user = current_user
-    # if @product.save!
-    #   redirect_to newest_show_path(@product), notice: "Product has been successfully added to our database"
-    # else
-    #   render :new
-    # end
-        # binding.pry
-
-
-    # session[:product_params].deep_merge!(params_product) if params_product
-    # authorize @product = Product.new(session[:product_params])
-    # @product.current_step = session[:product_step]
-    # if @product.valid?
-    #   if params[:back_button]
-    #     @product.previous_step
-    #   elsif @product.last_step?
-    #     @product.user = current_user
-    #     # binding.pry
-    #     if @product.all_valid?
-    #       @product.save!
-    #       flash[:notice] = 'Your product was created successfully'
-    #       redirect_to newest_products_path && return
-    #     end
-    #   else
-    #     @product.next_step
-    #   end
-    # end
-    # session[:product_step] = @product.current_step
-
-    # if @product.new_record?
-    #   return render :new
-    # else
-    #   session[:product_step] = session[:product_params] = nil
-    # end
-
     session[:product_params].deep_merge!(params_product) if params_product
     authorize @product = Product.new(session[:product_params])
     @product.current_step = session[:product_step]
@@ -94,18 +58,64 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    # @categories = policy_scope(Category)
+    # authorize @product = Product.find(params[:id])
     @categories = policy_scope(Category)
+    session[:product_params] ||= {}
     authorize @product = Product.find(params[:id])
+    @product.current_step = session[:step_product]
+    @product.user = current_user
   end
 
   def update
+    # authorize @product = Product.find(params[:id])
+    # if @product.update(params_product)
+    #   redirect_to newest_show_path(@product), notice: "Product has been successfully updated"
+    # else
+    #   # @product.errors
+    #   render :edit
+    # end
+    # @categories = policy_scope(Category)
+
+    @categories = policy_scope(Category)
+
     authorize @product = Product.find(params[:id])
-    if @product.update(params_product)
-      redirect_to newest_show_path(@product), notice: "Product has been successfully updated"
-    else
-      # @product.errors
-      render :edit
+    session[:product_params].deep_merge!(params_product) if params_product
+    @product.current_step = session[:step_product]
+    # @product.update(session[:product_params])
+    if params[:back_button]
+      @product.previous_step
+      redirect_to update_product_path(@product)
+    elsif @product.valid?
+      if @product.last_step?
+        # @product.user = current_user
+        # binding.pry
+        if @product.update(session[:product_params])
+          # @product.save!
+          flash[:notice] = "#{@product.name} was successfully updated within our Database"
+          redirect_to product_show_path(@product.category.id, @product)
+        end
+      else
+
+        @product.next_step
+        redirect_to update_product_path(@product)
+      end
     end
+      session[:step_product] = @product.current_step
+
+    if @product.new_record?
+      return render :edit
+    else
+      session[:step_product] = session[:product_params] = nil
+    end
+    # if @product.new_record?
+    #   return render :edit
+    # else
+    #   session[:step_product] = session[:product_params] = nil
+    #   flash[:notice] = "#{@product.name} was successfully added to the Marketplace"
+    #   redirect_to product_show_path(@product.category.id, @product)
+    # end
+
   end
 
   def destroy
