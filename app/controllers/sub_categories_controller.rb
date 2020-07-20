757e731325723e0f381dfae6ae29b7045d02ecf0
Istvan Lazar <istvanlaz@gmail.com>
2020-07-20 19:06:25 +0200

    search bar functional except for products_show

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -4,52 +4,20 @@ class SubCategoriesController < ApplicationController
   before_action :set_category
 
   def index
-    # @sub_categories = SubCategory.all
-    # @sub_categories = policy_scope(SubCategory)
     @sub_categories = policy_scope(@category.sub_categories).order(created_at: :desc)
   end
 
   def show
     @categories = policy_scope(Category)
-    authorize @sub_category = SubCategory.find(params[:id])
-    @sub_categories = policy_scope(@category.sub_categories)
-    @products = policy_scope(@sub_category.products).order(created_at: :desc)
-    # authorize @sub_category
-  end
 
-  def new
-    # authorize @sub_category = SubCategory.new
-    # @sub_category.user = current_user
-  end
-
-  def create
-    # authorize @sub_category = SubCategory.new(sub_category_params)
-    # @sub_category.user = current_user
-    # if @sub_category.save!
-    #   redirect_to sub_categories_path(@sub_category), notice: "sub_category was saved"
-    # else
-    #   render :new
-    # end
-  end
-
-  def edit
-    # authorize @sub_category = SubCategory.find(params[:id])
-  end
-
-  def update
-    # authorize @sub_category = SubCategory.find(params[:id])
-    # if @sub_category.update(sub_category_params)
-    #   redirect_to sub_categories_path, notice: "sub_category was updated"
-    # else
-    #   #@sub_category.errors
-    #   render :edit
-    # end
-  end
-
-  def destroy
-    # authorize @sub_category = SubCategory.find(params[:id])
-    # @sub_category.destroy
-    # redirect_to sub_categories_path, notice: "sub_category was destroyed"
+    if params[:query].present?
+      sql_query = "name @@ :query OR description @@ :query"
+      @products = Product.where(sql_query, query: "%#{params[:query]}%")
+    else
+      authorize @sub_category = SubCategory.find(params[:id])
+      @sub_categories = policy_scope(@category.sub_categories)
+      @products = policy_scope(@sub_category.products).order(created_at: :desc)
+    end
   end
 
   private
