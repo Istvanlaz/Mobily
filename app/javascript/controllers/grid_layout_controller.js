2b3e51292699f1235dc633857520738bcf1300dc
Istvan Lazar <istvanlaz@gmail.com>
2020-07-31 18:57:50 +0200

    layout grid for newest_index done -- currently working automation process for said layout

---
+++ app/javascript/controllers/grid_layout_controller.js
@@ -0,0 +1,74 @@
+// const productGrid = document.querySelector('.products-grid');
+// const products = Array.from(productGrid.children);
+// const productsLength = products.length;
+
+// console.log(products);
+
+
+
+// find the product with index 0
+// const firstProduct = products[0];
+// console.log(firstProduct);
+
+
+// find the product indexes that are multiples of 17
+// const multipleOfSeventeen = products[17];
+// console.log(multipleOfSeventeen);
+// const multiplesOfSeventeen = (numbers, multiple) => {
+//   return numbers.filter(x => x % multiple === 0);
+// }
+
+// const productIndexes = () => {
+
+// }
+
+// console.log(multiplesOfSeventeen([products.index], 17));
+
+// let number = 0;
+
+// for (let i = 17; i < products.length; i+=17) {
+//     number++;
+//     console.log(number);
+// }
+
+// let x = 0; let y = 17; let z = 9;
+
+// let number = 0;
+
+// console.log(productsLength);
+
+// for (let productIds = 0; productIds < productsLength; productIds++) {
+//   number+=productIds;
+//   console.log(number);
+// }
+
+
+
+// find the product indexes that are multiples of 9
+// const multipleOfNine = products[26];
+// console.log(multipleOfNine);
+
+
+
+// increment/change the grid-row attribute
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
