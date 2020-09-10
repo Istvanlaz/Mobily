a5a630d1c30eb472560c01821c1af495968a91d8
Istvan Lazar <istvanlaz@gmail.com>
2020-09-10 18:20:41 +0200

    created get_lucky view under best deals as a page where products 1.5km around the user will show up on a map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,6 +3,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 const mapElement = document.getElementById('map');
+const mapIndex = document.getElementById('map-index');
 
 // Building the map
 
@@ -10,6 +11,37 @@ if (mapElement) { // only build a map if there's a div#map to inject into
   mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
   const map = new mapboxgl.Map({
     container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+
+  const markers = JSON.parse(mapElement.dataset.markers);
+
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(new mapboxgl.Popup({offset: 5})
+                            .setHTML(marker.infoWindow.content))
+      .addTo(map);
+  })
+
+  if (markers.length === 0) {
+    map.setZoom(1);
+  } else if (markers.length === 1) {
+    map.setZoom(13);
+    map.setCenter([markers[0].lng, markers[0].lat]);
+  } else {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat]);
+    });
+    map.fitBounds(bounds, { duration: 2000, padding: 75 })
+  }
+}
+
+if (mapIndex) {
+  mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
+  const map = new mapboxgl.Map({
+    container: 'map-index',
     style: 'mapbox://styles/mapbox/streets-v10'
   });
 
