668dcb7a5722d2e8c0fb73a0ff0cc43fa07b3242
Arthur De Meyer <arthur.demeyer75@gmail.com>
2021-02-10 13:35:54 +0100

    setup user model for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: [:user, :editor, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
+
 
   has_one :wishlist
   has_one :shoping_cart
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_120426) do
+ActiveRecord::Schema.define(version: 2021_02_10_114124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -170,6 +170,7 @@ ActiveRecord::Schema.define(version: 2020_09_15_120426) do
     t.string "uid"
     t.float "latitude"
     t.float "longitude"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["product_id"], name: "index_users_on_product_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
