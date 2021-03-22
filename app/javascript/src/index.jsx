172a8cfdc651f4c9d75795fb822bfcd8a935a708
Istvan Lazar <istvanlaz@gmail.com>
2021-03-22 19:01:39 +0100

    heroku fix on master linked to -history- bug through react

--- app/javascript/src/index.jsx
+++ app/javascript/src/index.jsx
@@ -5,7 +5,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { createHistory as history } from 'history';
+//import { createHistory as history } from 'history';
 
 
 
@@ -13,7 +13,7 @@ import OrdersReducer from './reducers/orders_reducer';
 import OrdersIndex from './containers/orders_index';
 
 const reducers = combineReducers({
-  orders: OrdersReducer,
+  orders: OrdersReducer;
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
@@ -22,7 +22,7 @@ const middlewares = applyMiddleware(reduxPromise, logger);
 //the switch, for this route mount the ordersIndex Component
 ReactDOM.render(
   <Provider store={createStore(reducers, {}, middlewares)}>
-    <Router history={history}>
+    <Router>
       <Switch>
         <Route path="/orders" exact component={OrdersIndex} />
       </Switch>
